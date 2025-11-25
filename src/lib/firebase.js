1a33d8e7d11f711d483e7ce30fbd40b63ea46ac9
cmlund2610-dev <cm.lund2610@gmail.com>
2025-11-25 18:51:18 +0100

    Vercel Routing fix

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -10,10 +10,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// Determine the environment (development or production)
+const isProduction = window.location.hostname === 'sub-zero-eight.vercel.app';
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain: isProduction
+    ? 'sub-zero-eight.vercel.app'
+    : import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
